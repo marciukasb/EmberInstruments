77ccda9d7a7b4566a8a60a6157d78f5d17117a5a
marciukasb <martynasb1996@gmail.com>
2023-11-06 15:27:10 +0200

    Fix navigation

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -4,9 +4,7 @@ import styles from "../../styles/Navigation.module.scss";
 const Navigation = () => {
   return (
     <nav className={styles.navigation}>
-      <a href="/">
-        <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=306,fit=crop,q=95/YNqOjeJqkgtLP0Zw/ember-instruments-ig-white-AzG80ZlJ7es2xvZJ.jpg" />
-      </a>
+      <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=306,fit=crop,q=95/YNqOjeJqkgtLP0Zw/ember-instruments-ig-white-AzG80ZlJ7es2xvZJ.jpg" />
       <div className={styles.navigation__menu}>
         <NavLink href="/" name="Home" />
         <NavLink href="/about" name="About" />
