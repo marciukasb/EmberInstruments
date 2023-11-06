388f07d2e96dfe8fa6d9d93440105e8f23a1f2b0
marciukasb <martynasb1996@gmail.com>
2023-11-06 14:42:18 +0200

    Update navigation

--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -2,23 +2,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import styles from "../../styles/NavLink.module.scss";
 
 interface Props {
   href: string;
   name: string;
+  type?: string;
 }
 
 const NavLink = (props: Props) => {
   const pathname = usePathname();
 
+  const isActive = pathname === props.href;
+  console.log(styles);
+
   return (
-    <div>
-      <Link
-        className={`link ${pathname === "/" ? "active" : ""}`}
-        href={props.href}
-      >
-        {props.name}
-      </Link>
+    <div
+      className={`${styles.navlink} ${isActive && styles["navlink--active"]}`}
+    >
+      {props.type !== "anchor" && <Link href={props.href}>{props.name}</Link>}
+      {props.type === "anchor" && <a href={props.href}>{props.name}</a>}
     </div>
   );
 };
--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,25 +1,22 @@
 import NavLink from "./NavLink";
+import styles from "../../styles/Navigation.module.scss";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink href="/" name="Home" />
-        </li>
-        <li>
-          <NavLink href="/about" name="About" />
-        </li>
-        <li>
-          <NavLink href="/blog" name="Blog" />
-        </li>
-        <li>
-          <NavLink href="/contacts" name="Contacts" />
-        </li>
-        <li>
-          <a href="http://emberinstruments.shop/">Shop</a>
-        </li>
-      </ul>
+    <nav className={styles.navigation}>
+      <a href="/">
+        <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=306,fit=crop,q=95/YNqOjeJqkgtLP0Zw/ember-instruments-ig-white-AzG80ZlJ7es2xvZJ.jpg" />
+      </a>
+      <div className={styles.navigation__menu}>
+        <NavLink href="/" name="Home" />
+        <NavLink href="/about" name="About" />
+        <NavLink href="/contacts" name="Contacts" />
+        <NavLink
+          href="https://emberinstruments.shop"
+          name="Shop"
+          type="anchor"
+        />
+      </div>
     </nav>
   );
 };
