8ed992f897cc527672a60d9390cf7c289af94921
marciukasb <martynasb1996@gmail.com>
2026-05-10 15:41:08 +0300

    feat: switch blog list to read from local markdown files

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,55 +1,13 @@
-import Parser from "rss-parser";
-import BlogList from "../_components/BlogList/BlogList";
-import { BlogPost } from "../types/BlogPost";
-import { formatDate } from "../utils";
+import BlogList from '../_components/BlogList/BlogList';
+import { readAllPosts } from '../utils/blog';
 
 export const metadata = {
-  title: "Blog",
-  description: "Latest posts from Ember Instruments",
+  title: 'Blog',
+  description: 'Latest posts from Ember Instruments',
 };
 
-async function fetchBlogPosts(): Promise<Array<BlogPost>> {
-  const parser = new Parser();
-  const feed = await parser.parseURL(
-    "http://emberinstruments.siteleaf.net/feed.xml"
-  );
-
-  return feed.items.map((item) => {
-    let description =
-      item.content || item.contentSnippet || item.description || "";
-    description = description.replace(
-      /"thumbnail":\s*"\/uploads\//,
-      `"thumbnail": "http://emberinstruments.siteleaf.net/uploads/`
-    );
-    description = description.replace(
-      /src="\/uploads/g,
-      'class="blog-image" src="http://emberinstruments.siteleaf.net/uploads'
-    );
-
-    const startWord = "<Metadata>";
-    const endWord = "</Metadata>";
-
-    const startIndex = description.indexOf(startWord) + startWord.length;
-    const endIndex = description.indexOf(endWord, startIndex);
-
-    const metadataString = description.substring(startIndex, endIndex).trim();
-
-    let metadata = JSON.parse(metadataString) as any;
-
-    return {
-      title: item.title,
-      thumbnail: metadata.thumbnail,
-      summary: metadata.summary,
-      description,
-      topic: item.category || (item.categories && item.categories[0]),
-      date: formatDate(item.pubDate),
-    } as BlogPost;
-  });
-}
-
 export default async function Blog() {
-  const posts = await fetchBlogPosts();
-
+  const posts = await readAllPosts();
   return (
     <main>
       <h1>Blog</h1>
