3cd5376542a671553abcff497f895a94a7645a01
marciukasb <martynasb1996@gmail.com>
2026-05-10 20:43:44 +0300

    Remove subtitle

--- app/_components/HeroContent/HeroContent.tsx
+++ app/_components/HeroContent/HeroContent.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styles from "./HeroContent.module.scss";
+import React from 'react'
+import styles from './HeroContent.module.scss'
 
 const HeroContent = () => {
   const logoUrl =
-    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=306,fit=crop,q=95/YNqOjeJqkgtLP0Zw/ember-instruments-ig-white-AzG80ZlJ7es2xvZJ.jpg";
-  const description = "Lorem ipsum guitars for lorem ipsum players";
+    'https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=306,fit=crop,q=95/YNqOjeJqkgtLP0Zw/ember-instruments-ig-white-AzG80ZlJ7es2xvZJ.jpg'
+  const description = ''
   return (
     <div className={styles.heroContent}>
-      <img src={logoUrl} alt="Logo" className={styles.heroContent__logo} />
+      <img src={logoUrl} alt='Logo' className={styles.heroContent__logo} />
       <p className={styles.heroContent__description}>{description}</p>
     </div>
-  );
-};
+  )
+}
 
-export default HeroContent;
+export default HeroContent
